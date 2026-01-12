183822d29e9c6a6d63a1c7e6064fc36a7325aeb5
detrna <himselfunderscore@gmail.com>
2026-01-12 13:27:50 +0700

    gh-pages update

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "../index.html";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Route/Home";
 import Login from "./Route/Login";
